1b6118e52ae76114d57a80cedc9aeee6032bc6b8
Vih Damarques <vihdamarques@gmail.com>
2013-07-15 19:23:27 -0300

    primeira pagina usando bootstrap

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Controle Financeiro</title>
+        <!-- CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <style>
+        body {
+            padding-top: 60px;
+        }
+        .twitter {border: 1px #005580 dashed}
+        .twitter .twtr-hd {display:none !important;}
+        .twitter .twtr-timeline {font-family:arial !important; font-size:12pt !important;}
+
+        </style>
+        <!-- Scripts -->
+        <!-- script src="js/jquery-1.8.2.min.js"></script -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href="index.php">Controle Financeiro</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="index.php">Início</a></li>
+                            <li><a href="necessidade.php">Cad. de Necessidade</a></li>
+                            <li><a href="categoria.php">Cad. de Categoria</a></li>
+                            <li><a href="despesa.php">Lançamento de Despesa</a></li>
+                            <li><a href="relatorio.php">Relatório</a></li>
+                            <li><a href="sair.php">Sair</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <h1>Bem vindo ao sistema !</h1>
+            <p>Utilize o menu acima para navegar</p>
+        </div>
+    </body>
+</html>
