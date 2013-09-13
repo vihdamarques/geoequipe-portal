190ae2a6dc6288f1e9098fb5c7d34ea32a4077d5
Vih Damarques <vihdamarques@gmail.com>
2013-09-12 21:46:27 -0300

    Criado monitoramento e rastro

--- rastro.php
+++ rastro.php
@@ -14,7 +14,7 @@
         <div id="map-canvas"></div>
         <div id="controle" style="position:absolute; right:10px; top:50px; display:block; background-color:#FFF; padding:10px; text-align:right; border-radius:10px; opacity:0.8;">
             <select id="usuario">
-                <option value="0" selected>- Todos os usuários -</option>
+                <option value="0" selected>- Selecione o Usuário -</option>
                 <?php
                 foreach ($usuarioDAO->consultarTodos(0, 1000) as $usuario)
                     echo "<option value=\"" . $usuario->getId() . "\">" . $usuario->getNome() . "</option>";
@@ -22,6 +22,12 @@
                 $conn->__destruct();
                 ?>
             </select>
+            <br />
+            Data inicial: <input type="text" id="data_ini" class="input-small datepicker" placeholder="dd/mm/yyyy" value="11/09/2013">
+            <br />
+            Data final: <input type="text" id="data_fim" class="input-small datepicker" placeholder="dd/mm/yyyy" value="12/09/2013">
+            <br />
+            <button type="button" id="visualizar" class="btn">Visualizar</button>
         </div>
         <style>
             html, body, #map-canvas {
@@ -38,17 +44,22 @@
         <script src="js/GeoequipeAPI.js"></script>
         <script>
             geoequipe.criaMapa({div:"map-canvas"});
-            $("#usuario").change(carregaMarkers);
-            carregaMarkers();
-            setInterval("carregaMarkers()", 60000);
+            $("#visualizar").click(carregaRastro);
 
-            function carregaMarkers(){
+            function carregaRastro(){
               geoequipe.criaMarker({
-                processo: {processo: 'monitoramento', usuario: $("#usuario").val()}
+                processo: {processo: 'rastro', usuario: $("#usuario").val(), data_ini: $("#data_ini").val(), data_fim: $("#data_fim").val()}
                ,mapa: geoequipe.mapa(0)
-               ,limpar: geoequipe.marker()
+               ,limpar: geoequipe.sobreposicao()
               });
             }
+
+            $(".datepicker").datepicker({
+                language: "pt-BR",
+                orientation: "top",
+                format: "dd/mm/yyyy",
+                autoclose: true                
+            });
         </script>
     </body>
 </html>
